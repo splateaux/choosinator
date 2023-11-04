1f9577842464363005ec2f75353e43732c861c0f
John S Morton <splateaux@yahoo.com>
2023-11-04 16:44:50 -0400

    deleting everything

--- src/customTransport.js
+++
@@ -1,8 +0,0 @@
-export default function customTransport(options) {
-    return (record) => {
-      const logs = JSON.parse(localStorage.getItem('logs') || '[]');
-      logs.push(record);
-      localStorage.setItem('logs', JSON.stringify(logs));
-    };
-  }
-  
