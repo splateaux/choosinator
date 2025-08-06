2f8d2bbc17bfc1f5a129e666ac2117d9b7b82c43
John Morton <john.morton@example.com>
2025-08-05 20:46:38 -0400

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-    testDir: "./tests",
-    testIgnore: ["**/*.test.ts", "**/test/**"],
-    timeout: 30000,
-    expect: {
-        timeout: 5000,
-    },
-    reporter: "html",
-    use: {
-        actionTimeout: 0,
-        baseURL: "http://localhost:3333",
-        trace: "on-first-retry",
-    },
+  testDir: "./tests",
+  testIgnore: ["**/*.test.ts", "**/test/**"],
+  timeout: 30000,
+  expect: {
+    timeout: 5000,
+  },
+  reporter: "html",
+  use: {
+    actionTimeout: 0,
+    baseURL: "http://localhost:3333",
+    trace: "on-first-retry",
+  },
 });
