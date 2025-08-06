a97a6a4e1578db84259b5b77c6cdcacf6aaa3907
John Morton <john.morton@example.com>
2025-08-06 11:04:52 -0400

    test fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./test/setup-test-env.ts"],
-    exclude: ["tests/**/*", "**/*.spec.ts"],
+    exclude: ["tests/**/*", "**/*.spec.ts", "node_modules/**/*"],
   },
 });
